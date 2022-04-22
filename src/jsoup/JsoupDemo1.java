f96b5c255f98b19b936fbaac6184871695d53260
zhangxin <github.com/zhang-xin19>
2022-04-22 20:15:05 +0800

    'Test1'

---
+++ src/jsoup/JsoupDemo1.java
@@ -0,0 +1,11 @@
+package jsoup;
+
+import org.jsoup.Jsoup;
+
+public class JsoupDemo1 {
+    public static void main(String[] args) {
+        String path = JsoupDemo1.class.getClassLoader().getResource("student.xml").getPath();
+        Jsoup.parse()
+
+    }
+}
