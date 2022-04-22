f3a6b87c686ccb9ec51f7b800f3a38c77ea34b78
Zhang-Xin19 <104200269+Zhang-Xin19@users.noreply.github.com>
2022-04-22 22:51:29 +0800

    Update JsoupDemo1.java

--- src/jsoup/JsoupDemo1.java
+++ src/jsoup/JsoupDemo1.java
@@ -6,6 +6,5 @@ public class JsoupDemo1 {
     public static void main(String[] args) {
         String path = JsoupDemo1.class.getClassLoader().getResource("student.xml").getPath();
         Jsoup.parse()
-
     }
 }
